100f2b8cec6ad34c70550d354f5b8b4c615e02ac
gmassu <gmassu03@gmail.com>
2022-10-12 18:40:13 -0400

    Circle commit change

---
+++ circle2.js
@@ -0,0 +1,27 @@
+
+function setup() {
+    createCanvas(600, 400)
+    background(0)
+
+}
+
+px = 5
+py = 5
+
+vx = 5
+vy = 5
+
+radius=30
+function draw() {
+
+    background(0)
+
+    px = px + vx
+    py = py + vy
+
+    if (px > 600 || px < 0)(vx = -vx)
+
+    if (py > 400 ||py < 0)(vy = -vy)
+    
+     ellipse(px+vx, py+vy, radius, radius);
+}
